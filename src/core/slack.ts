c2e92da5fd78674cb8a65c623b384a8b32251604
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2023-01-06 10:06:46 +0000

    Refactor Slack message creation

--- src/core/slack.ts
+++ src/core/slack.ts
@@ -1,35 +1,34 @@
-import { WebClient } from "@slack/web-api";
+import { type Button, type SectionBlock, WebClient } from "@slack/web-api";
 
 import type { Repository } from "./types";
 
-export async function notifyChannel(
-	token: string,
-	channel: string,
-	{ repoName, repoUrl, userLogin, userName, userUrl }: Repository,
-): Promise<void> {
+export async function notifyChannel(token: string, channel: string, repository: Repository): Promise<void> {
 	const client = new WebClient(token);
 	await client.chat.postMessage({
 		blocks: [
-			{
-				type: "section",
-				text: {
-					type: "mrkdwn",
-					text: `A new repository <${repoUrl}|\`${repoName}\`> was just created by <${userUrl}|${userName ? userName : `\`${userLogin}\``}>.`,
-				},
-				accessory: {
-					action_id: "delete-repo",
-					type: "button",
-					text: {
-						type: "plain_text",
-						text: "🗑️Delete Repo",
-						emoji: true,
-					},
-					style: "danger",
-					url: `${repoUrl}/settings#danger-zone`,
-				},
-			},
+			repoSection(repository),
 		],
 		channel,
-		text: `A new repository was just created by ${userName ?? userLogin}`,
+		text: `A new repository was just created by ${repository.userName ?? repository.userLogin}`,
 	});
 }
+
+const repoSection = ({ repoName, repoUrl, userLogin, userName, userUrl }: Repository): SectionBlock => ({
+	type: "section",
+	text: {
+		type: "mrkdwn",
+		text: `A new repository <${repoUrl}|\`${repoName}\`> was just created by <${userUrl}|${userName ? userName : `\`${userLogin}\``}>.`,
+	},
+	accessory: deleteButton(`${repoUrl}/settings#danger-zone`),
+});
+
+const deleteButton = (url: string): Button => ({
+	type: "button",
+	action_id: "delete-repo",
+	style: "danger",
+	text: {
+		type: "plain_text",
+		text: "Delete repo",
+	},
+	url,
+});

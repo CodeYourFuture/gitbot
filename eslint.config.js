afda6cda156861c9052f899bdb30101c3a0ba496
Jonathan Sharpe <jonathan@codeyourfuture.io>
2024-10-31 18:33:31 +0000

    Move ESLint config from CLI to file

--- eslint.config.js
+++ eslint.config.js
@@ -9,6 +9,11 @@ export default [
 	{
 		ignores: ["lib/"],
 	},
+	{
+		linterOptions: {
+			reportUnusedDisableDirectives: "error",
+		},
+	},
 	cyfConfig,
 	...tsEslint.configs.strict,
 	...tsEslint.configs.stylistic,

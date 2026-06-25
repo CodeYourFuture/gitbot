fbbd733744f7b693165e2cf5d29137d5eff25834
Jonathan Sharpe <mail@jonrshar.pe>
2026-06-25 14:26:07 +0100

    Exclude type stubs from coverage

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		coverage: {
+			exclude: ["**/*.d.ts"],
 			include: ["src/**"],
 			reporter: ["html", "lcovonly", "text"],
 			reportsDirectory: "reports/coverage",

53afc5a59865afdf6f655e6f9bf4612b3d47d626
Jonathan Sharpe <jonathan@codeyourfuture.io>
2026-06-25 14:39:28 +0100

    Configure SonarQube scanning (#71)
    * Configure SonarQube scanning
    
    * Upgrade checkout action
    
        Make it non-shallow for better scan results.
    
    * Pin SonarQube action
    
    * Pin Netlify CLI
    
    * Don't run scripts on install
    
    * Add maintainability badge

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		coverage: {
+			exclude: ["**/*.d.ts"],
 			include: ["src/**"],
+			reporter: ["html", "lcovonly", "text"],
 			reportsDirectory: "reports/coverage",
 		},
 		globals: true,
